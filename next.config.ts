237145c73671ba18e53ae5b980f11e938ef23956
abdullahrm1511-pixel <abdullah.rm1511@gmail.com>
2026-07-04 20:34:08 +0200

    First Commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   ...(process.env.NEXT_DIST_DIR ? { distDir: process.env.NEXT_DIST_DIR } : {}),
+  outputFileTracingIncludes: {
+    "/*": ["./public/templates/inschrijf-formulier-stgbc.pdf"]
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "8mb"
